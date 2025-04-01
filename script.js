b63457058ee456cc2dff37f5ac8b71a86babc232
saritadipa34 <saritadipa34@gmail.com>
2025-04-01 15:18:21 +0530

    Updated css for the whole app

--- script.js
+++ script.js
@@ -14,16 +14,22 @@ save.addEventListener('click',()=>{
     const task=document.createElement('div');
     task.classList.add('task');
     container.appendChild(task);
+
+
     const paragraph=document.createElement('p');
     paragraph.classList.add('paragraph');
     task.appendChild(paragraph);
    paragraph.innerText=inputBox.value;
 const btnBox=document.createElement('div');
 task.appendChild(btnBox);
+
     const edit=document.createElement('button');
     edit.innerText="Edit";
     edit.classList.add('edit');
     btnBox.appendChild(edit);
+
+
+
     const deleteBtn=document.createElement('button');
     deleteBtn.innerText="Delete";
     deleteBtn.classList.add('deleteBtn');
